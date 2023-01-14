05236bf53ab2a0aa575f8d7edcff8409853f8719
James <5511220+Zamiell@users.noreply.github.com>
2023-01-13 22:51:24 -0500

    fix: hard coding file names

--- isaac_xml_validator.py
+++ isaac_xml_validator.py
@@ -17,179 +17,167 @@ class bcolors:
     UNDERLINE = "\033[4m"
 
 
+SCRIPT_PATH = os.path.realpath(__file__)
+SCRIPT_DIRECTORY = os.path.dirname(SCRIPT_PATH)
+XSD_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "xsd")
+
+
 # Configuration values from the end-user and/or GitHub Actions
-global rootFolder
-rootFolder = "**"
+global root_folder
+root_folder = "**"
 global recursive
 recursive = True
-global expectedErrorCount
-expectedErrorCount = 0
+global expected_error_count
+expected_error_count = 0
 
 # Other global variables
-global errorCount
+global error_count
 
-fileIgnoreList = [
+# Both of these files do not have a top-level tag, so they will always fail linting.
+# (The game's internal XML parser does not care about this.)
+file_ignore_list = [
     "fxlayers.xml",
     "seedmenu.xml",
 ]
 
-fileAllowList = [
-    "bossportraits.xml",
-    "entities2.xml",
-    "cutscenes.xml",
-    "itempools.xml",
-    "recipes.xml",
-    "costumes2.xml",
-    "bosspools.xml",
-    "music.xml",
-    "players.xml",
-    "wisps.xml",
-    "items_metadata.xml",
-    "nightmares.xml",
-    "locusts.xml",
-    "babies.xml",
-    "stages.xml",
-    "pocketitems.xml",
-    "challenges.xml",
-    "items.xml",
-    "bombcostumes.xml",
-    "curses.xml",
-    "sounds.xml",
-    "achievements.xml",
-    "backdrops.xml",
-    "giantbook.xml",
-    "bossoverlays.xml",
-]
 
-
-def clearIsaacRefsRecursive(node):
+def clear_isaac_refs_recursive(node):
     for child in list(node):
         if child.get("type") is not None:
             child.set("type", child.get("type").replace("xsisaac:", ""))
-        clearIsaacRefsRecursive(child)
+        clear_isaac_refs_recursive(child)
 
 
 def printf(*args):
     print(*args, flush=True)
 
 
-def printErr(string):
+def print_err(string):
     printf(bcolors.FAIL + str(string) + bcolors.ENDC)
 
 
-def printOK(string):
+def print_ok(string):
     printf(bcolors.OKGREEN + str(string) + bcolors.ENDC)
 
 
-def printWarn(string):
+def print_warn(string):
     printf(bcolors.WARNING + str(string) + bcolors.ENDC)
 
 
-def parseAndCheckSyntax(filename):
+def is_valid_xml(file_path):
+    """Perform a normal XML validation without any specific schema. This checks for e.g. closing tags and so on."""
     try:
-        return lxml.etree.parse(filename)
+        lxml.etree.parse(file_path)
+        return True
     except Exception as err:
-        printErr(err)
-        global errorCount
-        errorCount += 1
-        printErr("SYNTAX ERROR DETECTED!!")
-    return None
+        print_err(f'Failed to parse file "{file_path}": {err}')
+        return False
 
 
 def main():
-    global rootFolder, expectedErrorCount, recursive, errorCount
-    scriptPath = os.getcwd() + "/isaac-xml-validator/"
+    global root_folder, expected_error_count, recursive, error_count
 
-    totalErrorCount = 0
-    files = glob.glob(rootFolder + "/**.xml", recursive=recursive)
-    printf("Found " + str(len(files)) + " files in path: " + rootFolder + "/**.xml")
-    for filename in files:
-        filteredFilename = filename.split("\\")[len(filename.split("\\")) - 1]
-        filteredFilename = filteredFilename.split("/")[
-            len(filteredFilename.split("/")) - 1
-        ]
-        isValid = False
-        errorCount = 0
-        if filteredFilename in fileIgnoreList:
-            printWarn("Skip ignored xml file: " + filename)
+    total_error_count = 0
+
+    files = glob.glob(root_folder + "/**.xml", recursive=recursive)
+    printf(f"Found {len(files)} files in path: {root_folder}/**.xml")
+
+    for file_path in files:
+        error_count = 0
+
+        file_name = os.path.basename(file_path)
+        if file_name in file_ignore_list:
+            print_warn(f"Skipping ignored XML file: {file_path}")
             continue
 
-        if filteredFilename not in fileAllowList:
+        printf(f"Parsing XML file: {file_path}")
 
-            printf("Analyzing file as normal xml: " + filename)
-            isValid = parseAndCheckSyntax(filename) is not None
-        else:
+        # First, do a basic XML validation without any specific schema.
+        if not is_valid_xml(file_path):
+            total_error_count += 1
+            continue
 
-            printf("Analyzing Isaac xml file: " + filename)
+        # Check to see if we have a matching XSD file for this XML file.
+        xsd_file_name = file_name.replace(".xml", ".xsd")
+        xsd_path = os.path.join(XSD_DIRECTORY, xsd_file_name)
+        if not os.path.isfile(xsd_path):
+            continue
 
-            try:
-                xmlschema_root_doc = lxml.etree.parse(scriptPath + "isaacTypes.xsd")
-                xmlschema_doc = lxml.etree.parse(
-                    scriptPath + "xsd/" + filteredFilename.replace(".xml", ".xsd")
-                )
+        # Parse the XML file using our XSD file.
+        xml_errors = parse_isaac_xml_file(file_path, xsd_path)
+        if xml_errors is None:
+            continue
 
-                # Replace import node with content of the imported file, because lxml doesnt like https links
-                node = xmlschema_doc.getroot().find(
-                    "{http://www.w3.org/2001/XMLSchema}import"
-                )
-                if node is not None:
-                    for child in list(xmlschema_root_doc.getroot()):
-                        xmlschema_doc.getroot().insert(0, child)
-                    xmlschema_doc.getroot().remove(node)
-                clearIsaacRefsRecursive(xmlschema_doc.getroot())
-                xmlschema = lxml.etree.XMLSchema(xmlschema_doc)
+        for error in xml_errors:
+            print_err(
+                error.filename
+                + ":line "
+                + str(error.line)
+                + ":col "
+                + str(error.column)
+                + ": "
+                + error.message
+            )
+        error_count += len(xml_errors)
+        if error_count > 0:
+            printf(f"---- End errors for file: {file_path}")
 
-                xml_doc = parseAndCheckSyntax(filename)
-                if xml_doc is not None:
-                    isValid = xmlschema.validate(xml_doc)
-            except Exception as err:
-                printErr(err)
-        if not isValid:
-            for error in xmlschema.error_log:
-                printErr(
-                    error.filename
-                    + ":line "
-                    + str(error.line)
-                    + ":col "
-                    + str(error.column)
-                    + ": "
-                    + error.message
-                )
-            errorCount += len(xmlschema.error_log)
-            if errorCount > 0:
-                printf("---- End errors for file: " + filename)
-        else:
-            printOK("File is valid")
+        total_error_count += error_count
 
-        totalErrorCount += errorCount
-
-    printf("~~~~~ Finished analysing " + str(len(files)) + " files! ~~~~~")
-    if totalErrorCount > 0:
-        printErr("Found: " + str(totalErrorCount) + " Errors")
-        if int(totalErrorCount) != int(expectedErrorCount):
-            printErr("Expected error count was not reached!")
+    printf(f"~~~~~ Finished analyzing {len(files)} files! ~~~~~")
+    if total_error_count > 0:
+        print_err(f"Found: {total_error_count} errors")
+        if total_error_count != expected_error_count:
+            print_err("Expected error count was not reached!")
             sys.exit(1)
     else:
-        printOK("No errors found")
+        print_ok("No errors found")
 
 
-def readGithubEnvVars():
-    global rootFolder, expectedErrorCount, recursive
+def parse_isaac_xml_file(xml_file_path: str, xsd_file_path: str):
+    """Returns an array of errors or `None` if validation succeeded."""
+    try:
+        isaac_types_path = os.path.join(SCRIPT_DIRECTORY, "isaacTypes.xsd")
+        xml_schema_root_doc = lxml.etree.parse(isaac_types_path)
+        xml_schema_file_doc = lxml.etree.parse(xsd_file_path)
+
+        # Replace import node with content of the imported file, because lxml does not like HTTPS links.
+        node = xml_schema_file_doc.getroot().find(
+            "{http://www.w3.org/2001/XMLSchema}import"
+        )
+        if node is not None:
+            for child in list(xml_schema_root_doc.getroot()):
+                xml_schema_file_doc.getroot().insert(0, child)
+            xml_schema_file_doc.getroot().remove(node)
+
+        clear_isaac_refs_recursive(xml_schema_file_doc.getroot())
+        xml_schema = lxml.etree.XMLSchema(xml_schema_file_doc)
+        xml_doc = lxml.etree.parse(xml_file_path)
+        xml_schema.validate(xml_doc)
+        return None
+
+    except Exception as err:
+        print_err(err)
+        return xml_schema.error_log
+
+
+def read_github_env_vars():
+    global root_folder, expected_error_count, recursive
     printf("Evaluate settings:")
 
     if "INPUT_ROOTFOLDER" in os.environ:
-        rootFolder = os.environ["INPUT_ROOTFOLDER"]
-    printf("\tRoot folder: ", rootFolder)
+        root_folder = os.environ["INPUT_ROOTFOLDER"]
+    printf("\tRoot folder: ", root_folder)
 
     if "INPUT_RECURSIVE" in os.environ:
         recursive = os.environ["INPUT_RECURSIVE"]
     printf("\tRecursive: ", recursive)
 
     if "INPUT_EXPECTEDERRORCOUNT" in os.environ:
-        expectedErrorCount = os.environ["INPUT_EXPECTEDERRORCOUNT"]
-    printf("\tExpected Error Count: ", expectedErrorCount)
+        expected_error_count = os.environ["INPUT_EXPECTEDERRORCOUNT"]
+    printf("\tExpected Error Count: ", expected_error_count)
 
 
 if __name__ == "__main__":
-    readGithubEnvVars()
+    read_github_env_vars()
     main()
